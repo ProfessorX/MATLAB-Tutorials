7e640ee41639d04b45d0f378110146bf24c4fff3
Abraham Xiao <q289196573@gmail.com>
2013-04-17 17:54:24 +0800

    Updated_18:00

---
+++ Exp_02_02_01.m
@@ -0,0 +1,14 @@
+clear  % To gurantee the results are REPRODUCIBLE
+a = sin(3/10)  % DOUBLE
+sa = sym('sin(3/10)') 
+vpa(sa - a, 40)  % Calculate the error
+
+% This representation shall never be used,
+% Unless you would like to be fucked.
+sa1 = sym('sin(0.3)')
+sa2 = sym('sin(3e - 1)')
+d1 = vpa(sa - sa1, 40)  % Life is a bitch, why not fuck it, for FREE.
+
+% Now let's find specific data type.
+whos
+
